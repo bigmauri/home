54e30a85f9c4d914ca54aacf355645a30bd6013d
Maurizio <maurizio.bussi.mb@gmail.com>
2023-02-08 00:40:20 +0100

    my github page

--- pages.js
+++ pages.js
@@ -1,6 +1,6 @@
 const ghpages = require("gh-pages");
 const pathname = `${__dirname}/build`;
-const repoURL = "https://github.com/atmauriz/atmauriz.github.io.git";
+const repoURL = "https://github.com/bigmauri/bigmauri.github.io.git";
 
 ghpages.publish(
   pathname,
