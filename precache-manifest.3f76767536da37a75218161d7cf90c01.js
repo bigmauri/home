20455d44f536abc1a8eae17149539cbd756beb49
Maurizio <maurizio.bussi.mb@gmail.com>
2023-02-08 00:31:24 +0100

    Updates

---
+++ precache-manifest.3f76767536da37a75218161d7cf90c01.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "278cbe4bb42fc67241299afcc5a6c07a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fc4a9a6db1025b893e57",
+    "url": "/static/css/main.f5497eb8.chunk.css"
+  },
+  {
+    "revision": "afa716eb1a721f90822d",
+    "url": "/static/js/2.bda6e2cd.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.bda6e2cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc4a9a6db1025b893e57",
+    "url": "/static/js/main.7e0a1691.chunk.js"
+  },
+  {
+    "revision": "f52ba179432c3be28857",
+    "url": "/static/js/runtime-main.554504a0.js"
+  },
+  {
+    "revision": "6cc506d347be1acaa9743b5afeedcb17",
+    "url": "/static/media/picture.6cc506d3.jpg"
+  },
+  {
+    "revision": "e00ff54e3faf674d54668eea8fc03979",
+    "url": "/static/media/resume.e00ff54e.pdf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/home/precache-manifest.54c0f009cb860c5504eb94420c3e870e.js"
+  "/precache-manifest.3f76767536da37a75218161d7cf90c01.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/home/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
